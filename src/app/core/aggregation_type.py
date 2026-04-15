1aeccb479f1acd77dc4c0eb53cce6743920fb244
DariuszPasinski <245897@edu.p.lodz.pl>
2026-04-15 13:41:17 +0200

    Added basics of core class CERCAS

---
+++ src/app/core/aggregation_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class AggregationType(Enum):
+    MONTHLY = "MONTHLY"
+    QUARTERLY = "QUARTERLY"
---
+++ src/app/core/cercas.py
@@ -0,0 +1,63 @@
+from src.app.core.aggregation_type import AggregationType
+
+
+class CERCAS:
+    def __init__(self):
+        self.base: str | None = None
+        self.quote: str | None = None
+        self.start_date: str | None = None
+        self.end_date: str | None = None
+        self.aggregation_type: AggregationType | None = None
+        self.number_of_intervals: int | None = None
+
+    def set_pair(self, base: str, quote: str) -> None:
+        #TODO: Add checking correct format. ISO 4217
+        self.base = base.upper()
+        self.quote = quote.upper()
+
+    def set_period(self, start_date: str, end_date: str) -> None:
+        #TODO: Add checking date format. Should be yyyy-mm-dd.
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def set_type(self, type: AggregationType) -> None:
+        self.aggregation_type = type
+
+    def set_interval(self, number: int) -> None:
+        if number >= 0:
+            self.number_of_intervals = number
+        else:
+            raise ValueError("Interval must be positive")
+
+    def run_analysis(self) -> None:
+        pass
+
+    def export(self, file_path: str) -> None:
+        pass
+
+    def show_config(self) -> None:
+        if self.base is None:
+            print(f"Base currency isn't set.")
+            print(f"Quote currency isn't set.")
+        else:
+            print(f"Base currency is: {self.base}")
+            print(f"Quote currency is: {self.quote}")
+
+        if self.start_date is None:
+            print(f"Start date isn't set.")
+            print(f"End date isn't set.")
+        else:
+            print(f"Start date is: {self.start_date}")
+            print(f"End date is: {self.end_date}")
+
+        if self.aggregation_type is None:
+            print(f"Aggregation level for the analysis isn't set.")
+        elif self.aggregation_type == AggregationType.MONTHLY:
+            print(f"Aggregation level for the analysis is set to monthly.")
+        else:
+            print(f"Aggregation level for the analysis is set to quarterly.")
+
+        if self.number_of_intervals is None:
+            print(f"Number of intervals isn't set.")
+        else:
+            print(f"Number of intervals is: {self.number_of_intervals}")
