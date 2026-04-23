990ce29c5a26322bbf9bddffceb8cbcd0bbe916e
DariuszPasinski <245897@edu.p.lodz.pl>
2026-04-23 08:55:27 +0200

    Update currency_validator.py
    Got rid of url to table B and deleted not needed tabs

--- src/app/core/currency_validator.py
+++ src/app/core/currency_validator.py
@@ -2,8 +2,7 @@ import re
 import requests
 
 NBP_TABLE_URLS = [
-        "https://api.nbp.pl/api/exchangerates/tables/A?format=json",
-        "https://api.nbp.pl/api/exchangerates/tables/B?format=json"
+        "https://api.nbp.pl/api/exchangerates/tables/A?format=json"
     ]
 
 def validate_currency(code: str) -> None:
@@ -24,16 +23,15 @@ def is_iso4217_format(code: str) -> bool:
     return bool(ISO4217_REGEX.match(code))
 
 def fetch_all_nbp_codes() -> set[str]:
-    codes_a = fetch_nbp_codes(NBP_TABLE_URLS[0])
-    codes_b = fetch_nbp_codes(NBP_TABLE_URLS[1])
+    codes = fetch_nbp_codes(NBP_TABLE_URLS[0])
 
     # PLN is not listed in A/B tables (NBP base currency)
-    return codes_a.union(codes_b).union({"PLN"})
+    return codes.union({"PLN"})
 
 def fetch_nbp_codes(url: str) -> set[str]:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
 
-        data = response.json()
-        rates = data[0]["rates"]
-        return {rate["code"] for rate in rates}
+    data = response.json()
+    rates = data[0]["rates"]
+    return {rate["code"] for rate in rates}
